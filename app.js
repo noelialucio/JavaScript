3ea169d12d8518f5eb39beaac2d96981740d14af
Noelia Lucio <noe.lucio442@gmail.com>
2022-09-29 23:32:34 -0300

    librerias

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 //DECLARACION DE VARIABLES
 let total = 0;
-if(!localStorage.getItem('usuario')){
-let usuario = prompt("Ingresa tu nombre");
-const usuarioString = JSON.stringify(usuario)
-localStorage.setItem('usuario', usuarioString);
+if (!localStorage.getItem('usuario')) {
+    let usuario = prompt("Ingresa tu nombre");
+    const usuarioString = JSON.stringify(usuario)
+    localStorage.setItem('usuario', usuarioString);
 }
 
 let opcion;
-let pedido=[]
+let pedido = []
 let idCombos;
 let totalAMostrar = document.createElement("div");
 let pedidoAMostrar = document.createElement("h6");
 
 
 //CONSTRUCTOR
-class Combos{
-    constructor (nombre, precio, id) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.id = id ;
+class Combos {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
     }
 }
 
 //INSTANCIANDO OBJETOS
-const combo1 = new Combos("1 Pizza + 1 Bebida",1200, "1");
-const combo2 = new Combos("1 Docena de empanadas + 1 Bebida", 1700,"2");
+const combo1 = new Combos("1 Pizza + 1 Bebida", 1200, "1");
+const combo2 = new Combos("1 Docena de empanadas + 1 Bebida", 1700, "2");
 const combo3 = new Combos("2 Pizzas + 1 Bebida", 2200, "3");
-const combo4 = new Combos("1 Pizza + 1 Docena de empanadas + 1 Bebida",2500,"4");
+const combo4 = new Combos("1 Pizza + 1 Docena de empanadas + 1 Bebida", 2500, "4");
 
 
 
@@ -34,31 +34,59 @@ alert(`Bienvenidx a tu pizzeria favorita ${localStorage.getItem('usuario')} ! A 
 
 //EVENTOS 
 
-const comboUno = document.querySelector ("#ComboUno");
-comboUno.addEventListener ("click", ()=> {
-    elegirComida (combo1)
+const comboUno = document.querySelector("#ComboUno");
+comboUno.addEventListener("click", () => {
+    elegirComida(combo1);
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Agregaste el COMBO 1 a tu pedido!',
+        showConfirmButton: false,
+        timer: 1500
+    })
 });
 
-const comboDos = document.querySelector ("#ComboDos");
-comboDos.addEventListener ("click", ()=> {
-    elegirComida (combo2)
+const comboDos = document.querySelector("#ComboDos");
+comboDos.addEventListener("click", () => {
+    elegirComida(combo2);
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Agregaste el COMBO 2 a tu pedido!',
+        showConfirmButton: false,
+        timer: 1500
+    })
 });
 
-const comboTres = document.querySelector ("#ComboTres");
-comboTres.addEventListener ("click", ()=> {
-    elegirComida (combo3)
+const comboTres = document.querySelector("#ComboTres");
+comboTres.addEventListener("click", () => {
+    elegirComida(combo3);
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Agregaste el COMBO 3 a tu pedido!',
+        showConfirmButton: false,
+        timer: 1500
+    })
 });
 
 
-const comboCuatro = document.querySelector ("#ComboCuatro");
-comboCuatro.addEventListener ("click", ()=> {
-    elegirComida (combo4)
+const comboCuatro = document.querySelector("#ComboCuatro");
+comboCuatro.addEventListener("click", () => {
+    elegirComida(combo4);
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Agregaste el COMBO 4 a tu pedido!',
+        showConfirmButton: false,
+        timer: 1500
+    })
 });
 
 //AGREGAR COMIDA A PEDIDO
 
-function elegirComida(combo){
-    idCombos =  combo.nombre
+function elegirComida(combo) {
+    idCombos = combo.nombre
     total += combo.precio;
     pedido.push(idCombos)
     VerificarPedidoEnSessionStorage(pedido)
@@ -67,19 +95,19 @@ function elegirComida(combo){
     document.body.appendChild(totalAMostrar);
     document.body.appendChild(pedidoAMostrar);
     VerificarComboEnLocalStorage(combo)
- };
+};
 
- //GUARDANDO INFO EN STORAGE
- function VerificarPedidoEnSessionStorage(pedido){
+//GUARDANDO INFO EN STORAGE
+function VerificarPedidoEnSessionStorage(pedido) {
     const pedidoString = JSON.stringify(pedido)
     sessionStorage.setItem('combos', pedidoString);
-    const pedidoSessionStorage =  sessionStorage.getItem('combos');
+    const pedidoSessionStorage = sessionStorage.getItem('combos');
 }
 
-function VerificarComboEnLocalStorage(combo){
+function VerificarComboEnLocalStorage(combo) {
     const comboString = JSON.stringify(combo)
     localStorage.setItem(combo.id, comboString);
-    const comboLS =  localStorage.getItem(combo.id); 
+    const comboLS = localStorage.getItem(combo.id);
 }
 
 
@@ -87,40 +115,64 @@ function VerificarComboEnLocalStorage(combo){
 //LIMPIAR PEDIDO
 const vaciarCarrito = document.querySelector("#vaciarCarrito");
 vaciarCarrito.onclick = function () {
-   vaciar();
+    vaciar();
+    Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Eliminar tu pedido',
+        ConfirmButtonText: 'ELIMINAR',
+        
+    });
+   
+
 }
 
 //LIMPIAR PEDIDO
 const cerrarSesion = document.querySelector("#cerrarSesion");
 cerrarSesion.onclick = function () {
-   vaciar();
-   location.reload();
+    vaciar();
+    location.reload();
 }
 
 //FINALIZAR PEDIDO Y VACIAR CARRITO
 
-const finalizarPedido = document.querySelector ("#finalizarPedido");
-finalizarPedido.addEventListener ("click", ()=> {
-    alert("Tenes que abonar" + " " + total);
-    alert(`${localStorage.getItem('usuario')}, ingresa tus datos para el envio`);
-    direccion = prompt("Ingresa la direccion de envio");
-    telefono = prompt("Ahora ingresa tu numero de telefono");
-    alert(`${localStorage.getItem('usuario')}, en un rato recibiras tu comida en ${direccion}! Ante cualquier inconveniente te llamaremos al ${telefono}`);
-       
-    vaciar();
+const finalizarPedido = document.querySelector("#finalizarPedido");
+ finalizarPedido.addEventListener("click", async() => {
+   
+    const {
+        value: formValues
+    } = await Swal.fire({
+        title: `${localStorage.getItem('usuario')}, ingresa tus datos para el envio`,
+        html: '<input id="swal-input1" class="swal2-input">' +
+              '<input id="swal-input2" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+            return [
+                document.innerHTML= 'Enviaremos tu pedido a ',
+                document.getElementById('swal-input1').value,
+                document.getElementById('swal-input2').value,
+            ]
+        }
+    })
 
+    if (formValues) {
+        Swal.fire(JSON.stringify(formValues))
+    }
+   
+
+    vaciar();
 });
 
 
 //FUNCION REUTILIZABLE PARA VACIAR CARRITO
-function vaciar(){
-    total=0;
+function vaciar() {
+    total = 0;
     pedido = [];
     totalAMostrar.innerHTML = `<h3>Total: $ ${total}</h3>`
     pedidoAMostrar.innerHTML = 'No selecciono ningun combo.'
     document.body.appendChild(totalAMostrar);
     document.body.appendChild(pedidoAMostrar);
     localStorage.clear();
-    sessionStorage.clear ();
+    sessionStorage.clear();
+   
 }
-
